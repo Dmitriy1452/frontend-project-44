472024953b0902fe3e75da6d4247ee69cd89833a
Admin <tolstyachokhenkov@yandex.ru>
2025-06-14 23:35:37 +0300

    describe full logic of game

--- src/engine.js
+++ src/engine.js
@@ -1,12 +1,29 @@
 import readlineSync from 'readline-sync';
+import { name } from '../bin/brain-even.js';
 
 export const getName = () => {
     const askForName = readlineSync.question('May i have your name? ');
     return askForName;
 };
 
-export const isEven = () => {
-    const askForNumber = readlineSync.question('Answer "yes" if the number is even otherwise "no". ');
-    const guessTheNumber = (askForNumber % 2 === 0) ? 'Correct!' : 'Incorrect';
-    return guessTheNumber;
+const isEven = (num) => (num % 2 === 0) ? 'yes' : 'no'
+
+export const checkIfEven = () => {
+    let i = 3
+
+    while (i > 0) { 
+        const randomNumber =  Math.round(Math.random() * 100);
+        const correctAnswer = isEven(randomNumber);
+        
+        const question = readlineSync.question(
+            `Answer "yes" if the number is even, otherwise answer "no".\nQuestion: ${randomNumber}\nYour answer: `
+        ).toLowerCase().trim();
+    
+    if (question !== correctAnswer) {
+        return `'${question}' is wrong answer ;(. Correct answer was '${correctAnswer}'\nLet's try again, ${name}!`;
+    }
+    console.log('Correct!');
+    i -= 1;
+    }
+    return `Congratulations, ${name}!`;
 };
