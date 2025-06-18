328e2a1d7f23cd59fa9bd6c249578d243c79bff8
Admin <tolstyachokhenkov@yandex.ru>
2025-06-18 18:26:52 +0300

    refactoring

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 import { getName, checkIfEven } from '../src/engine.js';
 
-console.log('Welcome to the Brain Games!');
-
 const name = getName();
 console.log(`Hello, ${name}!`);
 console.log(checkIfEven(name));
