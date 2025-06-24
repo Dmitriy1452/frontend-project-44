9c620634b0eea69903c2cb951ef66234ad8efc68
Admin <tolstyachokhenkov@yandex.ru>
2025-06-24 18:34:38 +0300

    refactoring

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import readlineSync from 'readline-sync';
 
 export const getName = () => {
-    const askForName = readlineSync.question('Welcome to the Brain Games!\nMay i have your name? ');
+    const askForName = readlineSync.question('Welcome to the Brain Games!\nMay I have your name? ');
     return askForName;
 };
 
